9d713e3c5480dfc45eb79feb3acaba0d1b008d02
johnmartinezc <john.martinez@codeimmersives.com>
2022-11-22 14:52:41 -0500

    -a

---
+++ main.js
@@ -0,0 +1,54 @@
+let button = document.querySelector('#button')
+let body = document.querySelector('body')
+
+let parent = document.createElement('div')
+parent.className = 'parent'
+let child = document.createElement('div')
+child.className = 'child'
+
+parent.appendChild(child)
+body.appendChild(parent)
+
+
+
+
+
+
+let hacker = async() =>{
+    let response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    let data = await response.json()
+    for (let i = 0; i < 100; i++) {
+        
+     fetch(`https://hacker-news.firebaseio.com/v0/item/${data[i]}.json?print=pretty`)
+     .then(res=>{
+        return res.json()
+     })
+     .then(newdata=>{
+        console.log(newdata)
+        
+        let newChild = document.createElement('div')
+        let newChildTwo = document.createElement('div')
+        let linkChild = document.createElement('a')
+        newChild.className = 'box zone green'
+
+        body.appendChild(newChild)
+        linkChild.href = newdata.url
+        linkChild.innerHTML = `${newdata.title}`
+        linkChild.setAttribute('target', '_blank')
+        newChild.appendChild(linkChild)
+
+
+        newChildTwo.innerHTML = `\nAuthor- ${newdata.by}`
+        newChildTwo.className = 'red'
+        newChildTwo.innerHTML = `\nScore-${newdata.score}`
+        newChildTwo.innerHTML = `\nComment-${newdata.descendants}`
+        body.appendChild(newChildTwo)
+
+     })
+    
+}
+
+
+}
+
+hacker()
